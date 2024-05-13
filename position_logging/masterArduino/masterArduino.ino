5d7f370fe4360967defcd4afc121c02a2a76dce4
tarunkm02 <46302386+tarunkm02@users.noreply.github.com>
2024-05-13 10:37:19 -0700

    Added I2C_Anything.h file

--- position_logging/masterArduino/masterArduino.ino
+++ position_logging/masterArduino/masterArduino.ino
@@ -1,18 +1,57 @@
+#include "motor.h"
 #include <Wire.h>
+#include <I2C_Anything.h>
 
-double data = 0; 
+/*
+  intialize motor class with inputs
+    - motorID
+    - aPin (encoder A channel)
+    - bPin (encoder B channel)
+    - pwmPin (E1 speed control)
+    - dirPin (M1 direction control)
+    - upperLimit in deg (for free ROM) [has to be positive]
+    - lowerLimit in deg (for free ROM) [has to be negative]
+    [optional]
+      - spring constant (default = 10) 
+      - damper ratio (default = 0.35)
+*/
+motor motor1(1, 2, 3, 10, 9, 90, -90, 5);
+// motor motor2(2, 19, 18, 10, 11, 30, -30);
+// motor motor3(3, 20, 21, 6, 7, 30, -30);
+
+const int SLAVE_ADDRESS = 9;
+volatile double data; 
+
 void setup() {
-  // put your setup code here, to run once:
-  Wire.begin(9);
-  Wire.onReceive(receiveEvent);
+    // Begin serial monitor
+    Wire.begin();
 
-}
+    // Change PWM pin frequency to 20kHz
+    // TCCR1B &= ~7;
+    // TCCR1B |= 1;
+    // TCCR2B &= ~7;
+    // TCCR2B |= 1;
+    // TCCR4B &= ~7;
+    // TCCR4B |= 1;
 
-void receiveEvent(int bytes) {
-  data = Wire.read();
+    // Initialize
+    motor1.begin(motor1.aPin, motor1.bPin);
+    // motor2.begin(motor2.aPin, motor2.bPin, motor2.pwmPin, motor2.dirPin);
+    // motor3.begin(motor3.aPin, motor3.bPin, motor3.pwmPin, motor3.dirPin);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println(data);
+    // Calculate initial shaft position
+    motor1.calcPosition();
+    // motor2.calcPosition();
+    // motor3.calcPosition();
+
+    // motor1.calcTorqueOutput();
+    // motor2.calcTorqueOutput();
+    data = motor1.position;
+    Wire.beginTransmission(SLAVE_ADDRESS);
+    I2C_writeAnything(data);
+    Wire.endTransmission();
+    delay(200);
+
 }
--- position_logging/slaveArduino/slaveArduino.ino
+++ position_logging/slaveArduino/slaveArduino.ino
@@ -1,53 +1,33 @@
-#include "motor.h"
 #include <Wire.h>
+#include <I2C_Anything.h>
 
-/*
-  intialize motor class with inputs
-    - motorID
-    - aPin (encoder A channel)
-    - bPin (encoder B channel)
-    - pwmPin (E1 speed control)
-    - dirPin (M1 direction control)
-    - upperLimit in deg (for free ROM) [has to be positive]
-    - lowerLimit in deg (for free ROM) [has to be negative]
-    [optional]
-      - spring constant (default = 10) 
-      - damper ratio (default = 0.35)
-*/
-motor motor1(1, 2, 3, 10, 9, 90, -90, 5);
-// motor motor2(2, 19, 18, 10, 11, 30, -30);
-// motor motor3(3, 20, 21, 6, 7, 30, -30);
+const int SLAVE_ADDRESS = 9;
+volatile boolean haveData = false;
+volatile double data;
 
 void setup() {
-    // Begin serial monitor
-    Serial.begin(9600);
-    Wire.begin();
-
-    // Change PWM pin frequency to 20kHz
-    // TCCR1B &= ~7;
-    // TCCR1B |= 1;
-    // TCCR2B &= ~7;
-    // TCCR2B |= 1;
-    // TCCR4B &= ~7;
-    // TCCR4B |= 1;
-
-    // Initialize
-    motor1.begin(motor1.aPin, motor1.bPin);
-    // motor2.begin(motor2.aPin, motor2.bPin, motor2.pwmPin, motor2.dirPin);
-    // motor3.begin(motor3.aPin, motor3.bPin, motor3.pwmPin, motor3.dirPin);
+  // put your setup code here, to run once:
+  Wire.begin(SLAVE_ADDRESS);
+  Wire.onReceive(receiveEvent);
+  Serial.begin(9600);
 }
 
 void loop() {
-    // Calculate initial shaft position
-    motor1.calcPosition();
-    // motor2.calcPosition();
-    // motor3.calcPosition();
-
-    // motor1.calcTorqueOutput();
-    // motor2.calcTorqueOutput();
-
-    Wire.beginTransmission(9);
-    Wire.write(motor1.position);
-    Wire.endTransmission
-
+  // put your main code here, to run repeatedly:
+  if (haveData){
+    Serial.print("Received motor 1 position: ");
+    Serial.println(data);
+    haveData = false;
+  }
 }
+
+void receiveEvent (int howMany)
+ {
+ if (howMany >= (sizeof data))
+   {
+   I2C_readAnything (data);
+   haveData = true;
+   }  // end if have enough data
+ }  // end of receiveEvent
+
+
