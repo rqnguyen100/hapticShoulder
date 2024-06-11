2d4e5089734ce02331a703e7d3ea3624cfc3eab1
Roger Nguyen <rqnguyen@ucsd.edu>
2024-06-11 15:31:22 -0700

    Removed comments

--- main/main.ino
+++ main/main.ino
@@ -21,12 +21,6 @@
       - spring constant (default = 10) 
       - damper ratio (default = 0.35)
 */
-
-// motor test(1, 1, 8, 9, 10, 11, 12, 7, 3, 4, 5, 45, -45);
-// motor mujBig(   1, 10./3,  2,  4,  5,  6, 0, 0,  8, 31, 43, 20, -20); 
-// motor separateJ(2,     1, 18, 17, 16, 15, 0, 0, 10, 35, 41, 45, -45);   
-// motor mujSmall( 3, 10./3, 19, 20, 21, 22, 0, 0,  9, 33, 39, 20, -20);  
-
 motor oneDOF(   1,     1,  2,  3,  4,  5, 0, 0,  9, 10, 11, 30, -30);
  
 unsigned long timestamp;
@@ -54,29 +48,14 @@ void setup() {
     */
 
     // Initialize Pins
-    // test.begin(test.aPin, test.bPin, test.invAPin, test.invBPin, test.upperLimitPin, test.lowerLimitPin, test.pwmPin, test.dir1Pin, test.dir2Pin);
-    // mujBig.begin(mujBig.aPin, mujBig.bPin, mujBig.invAPin, mujBig.invBPin, mujBig.upperLimitPin, mujBig.lowerLimitPin, mujBig.pwmPin, mujBig.dir1Pin, mujBig.dir2Pin);
-    // separateJ.begin(separateJ.aPin, separateJ.bPin, separateJ.invAPin, separateJ.invBPin, separateJ.upperLimitPin, separateJ.lowerLimitPin, separateJ.pwmPin, separateJ.dir1Pin, separateJ.dir2Pin);
-    // mujSmall.begin(mujSmall.aPin, mujSmall.bPin, mujSmall.invAPin, mujSmall.invBPin, mujSmall.upperLimitPin, mujSmall.lowerLimitPin, mujSmall.pwmPin, mujSmall.dir1Pin, mujSmall.dir2Pin);
     oneDOF.begin(oneDOF.aPin, oneDOF.bPin, oneDOF.invAPin, oneDOF.invBPin, oneDOF.upperLimitPin, oneDOF.lowerLimitPin, oneDOF.pwmPin, oneDOF.dir1Pin, oneDOF.dir2Pin);
-
-    // Calibrate Position
-    // mujBig.calibratePosition();
 }
 
 void loop() {
     // Calculate position output
-    // test.calcPosition();
-    // mujBig.calcPosition();
-    // separateJ.calcPosition();
-    // mujSmall.calcPosition();
     oneDOF.calcPosition();
 
     // Do haptics
-    // test.calcTorqueOutput();
-    // mujBig.calcTorqueOutput();
-    // separateJ.calcTorqueOutput();
-    // mujSmall.calcTorqueOutput();
     oneDOF.calcTorqueOutput();
 
     // // Serial.println(separateJ.position);
